a8ad7f76475dba345a9562d4983b085989cf9218
MosesAbuya <115994128+MosesAbuya@users.noreply.github.com>
2026-06-30 09:01:01 +0300

    Db creds

--- includes/db_credentials.php
+++ includes/db_credentials.php
@@ -5,8 +5,13 @@
 // This file should ideally be placed OUTSIDE the web root in a production environment
 // to prevent direct access via URL.
 
-define('DB_HOST', '127.0.0.1');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'lerne');
+// define('DB_HOST', '127.0.0.1');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'lerne');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'faridagi_lerne');
+define('DB_PASS', 'Lernr@2026');
+define('DB_NAME', 'aridagi_lerne');
 ?>
